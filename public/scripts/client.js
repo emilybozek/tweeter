02bd9f1a3d89574a99f44bc84e59f180f766e5e4
feltfan <emily.bozek@gmail.com>
2022-03-31 18:34:41 -0700

    auto refresh tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
+  // Submit Handler
+  $(".tweet-form").on('submit', function(event) {
+    event.preventDefault();
+    console.log($(this).serialize());
+    if ($("#tweet-text").val().length === '') {
+      return showToastError(toastError("Tweet cannot be empty!"));
+    }
+    if ($(".counter").val() < 0) {
+      return showToastError(toastError(`Tweet cannot exceed ${MAX_TWITTER_CHAR_COUNT} characters!`));
+    }
+    $.ajax( { 
+      method: 'POST',
+      url: "/tweets",
+      data: $(this).serialize()
+    })
+    .then(() => {
+      loadTweets();
+    });
+  });
+  // Loader
+  const loadTweets = function() {
+    $.ajax({
+      type: "GET",
+      url: "/tweets",
+      data: "data"
+    })
+      .then((data) => {
+      // $("#tweet-input").val('');
+      // $(".counter").val('140');
+      // $(".tweets-container").empty();
+      renderTweets(data);
+      });
+  };
+    loadTweets();
+  });
+  
 
 // Element Creator
 const createTweetElement = function(tweet) {
@@ -13,6 +49,9 @@ const createTweetElement = function(tweet) {
     </header>
     <div class="tweet-body">${tweet.content.text}</div>
     <footer class="tweet-footer">
+      <output class="tweet-time" for="tweet-text">
+      ${timeago.format(tweet.created_at)}
+      </output>
       <div class="tweet-actions">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -25,35 +64,29 @@ const createTweetElement = function(tweet) {
 }
 
 // Renderer
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-    let allTweets = createTweetElement(tweet);
-    $('.tweets-container').prepend(allTweets);
-  }
- }
-
-// Loader
-const loadTweets = function() {
-  $.ajax("/tweets").then((tweets) => {
-    $(".tweets-container").empty();
-    renderTweets(tweets);
+const renderTweets = function (tweets) {
+  tweets.forEach((element) => {
+    $(".tweets-container").prepend(createTweetElement(element));
   });
 };
 
-// Submit Handler
-$(".tweet-form").submit(function(event) {
-  event.preventDefault();
-  let newTweet = $("#tweet-text").serialize();
-  $.ajax({
-    type: "POST",
-    url: "/tweets",
-    data: newTweet,
-  })
-  // For Additional Tweets
-  loadTweets()
-})
+ const toastError = (errorMessage) => {
+  return $(`
+  <section class="toast-error" data-id="toast-error">
+    <i class="fa-solid fa-circle-exclamation toast-content" id="error-icon"></i>
+    <div class="error-message toast-content"><strong>Error: </strong>${errorMessage}</div>
+    <i class="fa-solid fa-xmark toast-content" id="error-close" data-id="error-close"></i>
+  </section>
+  `);
+};
 
-// For Initial Page
-loadTweets()
-
-});
+const showToastError = (toastError) => {
+  if ($(dataTags.error).length > 0) {
+    return;
+  }
+  $(dataTags.nav).append(toastError);
+  $(dataTags.errorClose).on('click', function(event) {
+    event.preventDefault();
+    $(dataTags.error).remove();
+  });
+};
